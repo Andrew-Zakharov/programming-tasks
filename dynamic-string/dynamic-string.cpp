d25563759aaeb12f92d138c75963b01bfc6539ec
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-05-13 15:20:47 +0200

    Added dynamic-string project

---
+++ dynamic-string/dynamic-string.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+char* concatenate(const char* first, const char* second)
+{
+    return nullptr;
+}
+
+char* append(const char* str, char c)
+{
+    return nullptr;
+}
+
+char charAt(const char* str, unsigned int index)
+{
+    return 0;
+}
+
+void test_dynamic_string()
+{
+
+}
+
+int main()
+{
+    return 0;
+}
