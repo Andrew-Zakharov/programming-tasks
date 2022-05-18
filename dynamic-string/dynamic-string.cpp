9685e2d14994708ee2fb955deeb146f2a2b18094
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-05-18 11:18:36 +0200

    Save function result to its argument

--- dynamic-string/dynamic-string.cpp
+++ dynamic-string/dynamic-string.cpp
@@ -2,7 +2,6 @@
 
 #define NULL_SYMBOL_SIZE 1
 
-//size with null symbol
 unsigned int get_size(const char* str)
 {
     unsigned int size = 0;
@@ -19,20 +18,20 @@ unsigned int get_size(const char* str)
     return size;
 }
 
-char* concatenate(const char* first, const char* second)
+void concatenate(char** first, const char* second)
 {
     char* result = nullptr;
     unsigned int i = 0;
     unsigned int j = 0;
 
-    const unsigned int first_size = get_size(first);
+    const unsigned int first_size = get_size(*first);
     const unsigned int second_size = get_size(second);
 
     result = new char[first_size + second_size + NULL_SYMBOL_SIZE];
 
     for (i = 0; i < first_size; i++)
     {
-        result[i] = first[i];
+        result[i] = *(*first + i);
     }
 
     for (j = 0; j < second_size; j++)
@@ -42,26 +41,34 @@ char* concatenate(const char* first, const char* second)
 
     result[i] = '\0';
 
-    return result;
+    delete* first;
+    *first = result;
+
+    result = nullptr;
 }
 
-char* append(const char* str, char c)
+void append(char** str, char c)
 {
     char* result = nullptr;
-    const unsigned int size = get_size(str);
+    const unsigned int size = get_size(*str);
     unsigned int i = 0;
 
     result = new char[size + 1 + NULL_SYMBOL_SIZE];
 
-    for (i = 0; i < size; i++)
+    if (result)
     {
-        result[i] = str[i];
+        for (i = 0; i < size; i++)
+        {
+            result[i] = *((*str) + i);
+        }
+
+        result[i++] = c;
+        result[i] = '\0';
+
+        delete *str;
+        *str = result;
+        result = nullptr;
     }
-
-    result[i++] = c;
-    result[i] = '\0';
-
-    return result;
 }
 
 char charAt(const char* str, unsigned int index)
@@ -69,14 +76,37 @@ char charAt(const char* str, unsigned int index)
     return str[index];
 }
 
+char* initDynamicArray(const char* str)
+{
+    char** p = nullptr;
+    const unsigned int size = get_size(str);
+
+    p = new char*;
+    *p = new char[size + NULL_SYMBOL_SIZE];
+
+    if (*p)
+    {
+        unsigned int i = 0;
+
+        for (i = 0; i < size; i++)
+        {
+            *(*p + i) = str[i];
+        }
+
+        *(*p + i) = '\0';
+    }
+
+    return *p;
+}
+
 void test_dynamic_string()
 {
-    const char* s1 = "abc";
-    const char* s2 = "def";
+    char* s1 = initDynamicArray("abc");
+    char* s2 = initDynamicArray("def");
 
-    std::cout << get_size(s1) << std::endl;
-    std::cout << concatenate(s1, s2) << std::endl;
-    std::cout << append(s1, 'd');
+    concatenate(&s1, s2);
+
+    std::cout << s1;
 }
 
 int main()
