500088b0e9e9f7b3ccba1f3d8f4817ba18744813
Andrew-Zakharov <andrey.zakharow@keemail.me>
2022-06-24 14:16:47 +0200

    bubble sort

--- sort/sort.cpp
+++ sort/sort.cpp
@@ -1,20 +1,35 @@
-// sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+
+namespace sort {
+    void bubble(std::vector<int>& source) {
+        for (size_t i = 0; i < source.size(); i++){
+            for (size_t j = i + 1; j < source.size(); j++) {
+                if (source.at(i) > source.at(j)) {
+                    std::swap(source[i], source[j]);
+                }
+            }
+        }
+    }
+}
+
+void print_vector(const std::vector<int> v) {
+    for (auto n : v) {
+        std::cout << n << ' ';
+    }
+
+    std::cout << std::endl;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> numbers = { 99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0 };
+
+    print_vector(numbers);
+
+    sort::bubble(numbers);
+
+    print_vector(numbers);
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
